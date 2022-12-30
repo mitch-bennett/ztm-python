268f74f2ba07e1c386a95e7e64b652048bb1f2eb
Mitch Bennett <mitch.f.bennett@gmail.com>
2022-12-30 09:23:03 -0800

    add additional image script examples

--- Scripting/Images/images.py
+++ Scripting/Images/images.py
@@ -20,6 +20,36 @@ filtered_img.save("blur.png", "png")
 filtered_img = img.filter(ImageFilter.SHARPEN)
 filtered_img.save("sharpen.png", "png")
 
-# convert to different colorspace
+# convert to different colorspace. show will display image.
 bw_img = img.convert("L")
 bw_img.save("grey.png", "png")
+# bw_img.show()
+
+# can also rotate images
+filtered_img = img.convert("L")
+crooked = filtered_img.rotate(180)
+crooked.save("rotate.png", "png")
+
+# resizing image takes a tuple for w, h
+filtered_img = img.convert("L")
+tiny = filtered_img.resize((300, 300))
+tiny.save("tiny.png", "png")
+
+# cropping takes a box tuple and apply to region
+filtered_img = img.convert("L")
+box = (100, 100, 400, 400)
+region = filtered_img.crop(box)
+region.save("cropped.png", "png")
+
+# take massive astro image and make smaller
+# pay attention to aspect ratio, which is not locked by default
+img = Image.open("./astro.jpg")
+new_img = img.resize((400, 400))
+new_img.save("astro_thumbnail.jpg")
+
+# this will apply correct scaling. thumbnail modifies current file
+# and does not require writing to a new variable
+img = Image.open("./astro.jpg")
+img.thumbnail((400, 400))
+new_img.save("astro_thumbnail-2.jpg")
+print(img.size)
