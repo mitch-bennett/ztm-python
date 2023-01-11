a07c85e04fe4e969875c3b2a81fb237600414d17
Mitch Bennett <mitch.f.bennett@gmail.com>
2023-01-11 07:13:34 -0800

    add ML exercises in progress

---
+++ MachineLearning/ML Steps.py
@@ -0,0 +1,21 @@
+# 1 - Import the data (csv file, etc)
+# 2 - Clean the data
+# 3 - Split data into training and test sets
+# 4 - Create a model from training set
+# 5 - Check the output with test set
+# 6 - Improve
+
+"""
+tools:
+numpy
+pandas
+scikit-learn
+matplotlib
+
+resources:
+https://elitedatascience.com/python-cheat-sheet
+https://github.com/GokuMohandas/Made-With-ML
+https://www.kaggle.com/datasets/javagarm/fifa-19-complete-player-dataset
+https://docs.python.org/3/library/codecs.html#standard-encodings
+
+"""
