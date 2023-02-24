9629f854677c3d2bf7adc50d67cb379461083eeb
Mitch Bennett <mitch.f.bennett@gmail.com>
2023-02-23 20:33:44 -0800

    built first flask server and walkthrough

--- WebDevelopment/webserver/server.py
+++ WebDevelopment/webserver/server.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
 # @app.route('/')
 # def hello_world():
 #     return 'Hello, MITCH!'
+
 
 @app.route('/blog')
 def blog():
@@ -14,10 +15,19 @@ def blog():
 def blog2():
     return 'This is my dog'
 
-@app.route('/')
-def hello_world():
-    return render_template('./index.html')
+# @app.route('/')
+# def hello_world():
+#     print(url_for('static', filename='favicon.ico'))
+#     return render_template('./index.html')
+
+@app.route('/<username>/<int:post_id>')
+def hello_world(username=None, post_id=None):
+    return render_template('./index.html', name=username, post_id=post_id)
 
 @app.route('/about.html')
 def about():
     return render_template('./about.html')
+
+# @app.route('/favicon.ico')
+# def fav():
+#     return 'These are my thoughts on blogs'
