5c598e795dd39c9d0a1caec39c76df3d7b85910f
Mitch Bennett <mitch.f.bennett@gmail.com>
2023-02-26 21:16:23 -0800

    added submit_form route for contact page

--- WebDevelopment/webportfolio/portfolio.py
+++ WebDevelopment/webportfolio/portfolio.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +8,13 @@ def my_home():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong. Try again!'
+
